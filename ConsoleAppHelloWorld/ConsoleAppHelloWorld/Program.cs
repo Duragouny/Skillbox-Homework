574b38c59cb83f5b3cadfcf97a3e9e2191101b3f
Duragouny <lev.shabatin2011@gmail.com>
2024-05-02 14:19:47 +0500

    HelloWorld

---
+++ ConsoleAppHelloWorld/ConsoleAppHelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console; //Убираем необходимость приписывать "Console" перед каждым оператором
+
+namespace ConsoleAppHelloWorld
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Ого, это что, XML комментарий?
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!!!");
+
+            //Пишем то же самое, но с помощью Write
+            Write("Hello, ");
+            Write("World");
+            Write("!!!");
+
+            WriteLine();
+            WriteLine("Нажмите любую кнопку, чтобы выйти из консоли:");
+            ReadKey();
+        }
+    }
+}
